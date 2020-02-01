0ea63bdde1d7c7b83d7e5b972e9bac734c0d5c6a
Marcelo Barsotti <mbarsott@gmail.com>
2020-02-01 18:35:19 -0500

    2. Writing Your First NUnit Test

---
+++ Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,16 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    class LoanTermShould
+    {
+        [Test]
+        public void ReturnTemInMonths()
+        {
+            var sut = new LoanTerm(1);
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
+    }
+}
